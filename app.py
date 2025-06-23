01fc5a9b8f8d6cfa5d1e500f3b7dd8385af1fe6c
Abdur Rahaman <abdurasm112@gmail.com>
2025-06-23 12:46:50 +0530

    Working Version:MongoDB connection,simulated energy data,and live dashboard

--- app.py
+++ app.py
@@ -1,22 +1,43 @@
 from flask import Flask, request, jsonify  # type: ignore
 from flask_cors import CORS  # type: ignore
+from pymongo import MongoClient  # type: ignore
+from bson import ObjectId  # type: ignore
+import os
+
 app = Flask(__name__)
 CORS(app)
 
-# Health check route
+# MongoDB setup
+# replace with actual MongoDB URI
+client = MongoClient(
+    "mongodb+srv://energy-database:Ab%40629662%402425@energy-cluster.xms2gg8.mongodb.net/?retryWrites=true&w=majority&appName=energy-cluster")
+db = client["energy_database"]
+collection = db["readings"]
+
+# Health check
 
 
 @app.route("/")
 def index():
     return "Energy Monitor Backend is running!"
+
 # POST route to receive readings
 
 
 @app.route("/readings", methods=["POST"])
-def add_readings():
+def add_reading():
     data = request.get_json()
-    print("Received readings:", data)
-    return jsonify({"status": "successs"}), 201
+    print("Received:", data)
+    collection.insert_one(data)
+    return jsonify({"status": "success"}), 201
+
+# GET route to send readings
+
+
+@app.route("/readings", methods=["GET"])
+def get_readings():
+    readings = list(collection.find({}, {"_id": 0}))
+    return jsonify(readings[::-1])  # reverse to show latest first
 
 
 if __name__ == "__main__":
--- mock_sensor.py
+++ mock_sensor.py
@@ -3,28 +3,24 @@ import random
 import time
 from datetime import datetime
 
-API_URL = "http://127.0.0.1:5000/readings"
+url = 'http://127.0.0.1:5000/readings'
 
-
-def generate_mock_data():
-    return {
-        "timestamp": datetime.now().isoformat(),
-        "current":
-        # random current between 0.5A and 5.0A
-        round(random.uniform(0.5, 5.0), 2)
-    }
+appliances = ['Fan', 'Light', 'AC', 'TV', 'Refrigerator']
 
 
 def send_data():
     while True:
-        data = generate_mock_data()
+        data = {
+            "appliance": random.choice(appliances),
+            "timestamp": datetime.now().isoformat(),
+            "watts": round(random.uniform(50, 500), 2)  # Simulating power
+        }
         try:
-            response = requests.post(API_URL, json=data)
-            print(f"sent: {data}|status:{response.status_code}")
+            response = requests.post(url, json=data)
+            print("Sent:", data, "Status:", response.status_code)
         except Exception as e:
             print("Error sending data:", e)
-        time.sleep(5)  # wait for 5 seconds before sending the next reading
+        time.sleep(5)
 
 
-if __name__ == "__main__":
-    send_data()
+send_data()
